e59a3210931becd618565f91e11a220a0ab703ba
mwillis1989 <105248701+mwillis1989@users.noreply.github.com>
2026-03-09 13:06:52 -0700

    Update light.js

--- js/light.js
+++ js/light.js
@@ -8,35 +8,27 @@ block.innerHTML=`
 <details>
 <summary>Lux → PPFD</summary>
 
-<label>Lux</label>
 <input id="lux">
-
 <button onclick="luxPPFD()">Convert</button>
 
 <div id="ppfdResult" class="result"></div>
 
 </details>
 
-
 <details>
 <summary>Joules/cm² → DLI</summary>
 
-<label>J/cm²</label>
 <input id="joule">
-
 <button onclick="jouleDLI()">Convert</button>
 
 <div id="dliResult" class="result"></div>
 
 </details>
 
-
 <details>
 <summary>Micromoles → Moles</summary>
 
-<label>µmol</label>
 <input id="umol">
-
 <button onclick="umolMol()">Convert</button>
 
 <div id="molResult" class="result"></div>
@@ -47,14 +39,7 @@ block.innerHTML=`
 
 });
 
-function luxPPFD(){
-ppfdResult.innerText=(lux.value/54).toFixed(2)+" µmol/m²/s";
-}
 
-function jouleDLI(){
-dliResult.innerText=(joule.value*10000/217000).toFixed(2)+" mol/m²/day";
-}
-
-function umolMol(){
-molResult.innerText=(umol.value/1000000)+" mol";
-}
+function luxPPFD(){ppfdResult.innerText=(lux.value/54).toFixed(2)+" µmol/m²/s";}
+function jouleDLI(){dliResult.innerText=(joule.value/21.7).toFixed(2)+" mol/m²/day";}
+function umolMol(){molResult.innerText=(umol.value/1000000)+" mol";}

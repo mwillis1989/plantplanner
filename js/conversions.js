abb0e9705127e19829a33395cea1d9a165ec2238
mwillis1989 <105248701+mwillis1989@users.noreply.github.com>
2026-03-09 12:44:37 -0700

    Update conversions.js

--- js/conversions.js
+++ js/conversions.js
@@ -33,12 +33,22 @@ block.innerHTML=`
 <div id="tempResult" class="result"></div>
 
 </details>
+<details>
+<summary>Imperial per Acre Conversion</summary>
 
-`
+<label>Value</label>
+<input id="acreVal">
+
+<button onclick="acreConvert()">Convert</button>
+
+<div id="acreResult" class="result"></div>
+
+</details>
 
 })
 
 function ft(){mResult.innerText=(ft.value*0.3048).toFixed(2)}
 function gal(){lResult.innerText=(gal.value*3.78541).toFixed(2)}
 function temp(){tempResult.innerText=((f.value-32)*5/9).toFixed(2)}
+function acreConvert(){acreResult.innerText=(acreVal.value/43560).toFixed(6)+" per ft²"}
 

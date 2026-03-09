9a7669f539d1091566cff6c4fdff1647eb25c453
mwillis1989 <105248701+mwillis1989@users.noreply.github.com>
2026-03-09 11:22:54 -0700

    Create light.js

---
+++ js/light.js
@@ -0,0 +1,49 @@
+const lightBlock=document.getElementById("calcBlock5");
+
+lightBlock.innerHTML=`
+
+<details>
+<summary>Lux → PPFD</summary>
+
+<label>Lux</label>
+<input id="luxInput">
+
+<button onclick="luxToPPFD()">Convert</button>
+
+<div id="ppfdResult" class="result"></div>
+
+</details>
+
+<details>
+<summary>Micromoles → Moles</summary>
+
+<label>µmol</label>
+<input id="umolInput">
+
+<button onclick="umolToMol()">Convert</button>
+
+<div id="molResult" class="result"></div>
+
+</details>
+
+`;
+
+function luxToPPFD(){
+
+const lux=parseFloat(document.getElementById("luxInput").value);
+
+const ppfd=lux/54;
+
+document.getElementById("ppfdResult").innerText=ppfd.toFixed(2)+" µmol/m²/s";
+
+}
+
+function umolToMol(){
+
+const umol=parseFloat(document.getElementById("umolInput").value);
+
+const mol=umol/1000000;
+
+document.getElementById("molResult").innerText=mol+" mol";
+
+}

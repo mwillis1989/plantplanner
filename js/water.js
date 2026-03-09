1cfe6a6cda3c1e296dc9bcb54c9e47a46eacb8ef
mwillis1989 <105248701+mwillis1989@users.noreply.github.com>
2026-03-09 13:14:29 -0700

    Update water.js

--- js/water.js
+++ js/water.js
@@ -8,14 +8,14 @@ block.innerHTML=`
 <details>
 <summary>Hydrogen Peroxide Dilution</summary>
 
-<label>Water Volume L</label>
-<input id="vol">
+<label>Water Volume (L)</label>
+<input id="waterVol">
 
 <label>Target ppm</label>
-<input id="ppm">
+<input id="targetPPM">
 
 <label>Peroxide %</label>
-<input id="strength">
+<input id="peroxidePercent">
 
 <button onclick="peroxide()">Calculate</button>
 
@@ -23,11 +23,50 @@ block.innerHTML=`
 
 </details>
 
+
+<details>
+<summary>Irrigation Water Requirement</summary>
+
+<label>Area m²</label>
+<input id="waterArea">
+
+<label>L per m²</label>
+<input id="waterRate">
+
+<button onclick="waterUse()">Calculate</button>
+
+<div id="waterResult" class="result"></div>
+
+</details>
+
+
+<details>
+<summary>Annual Water Storage</summary>
+
+<label>Storage m³</label>
+<input id="storage">
+
+<label>Days</label>
+<input id="days">
+
+<button onclick="waterStorage()">Calculate</button>
+
+<div id="storageResult" class="result"></div>
+
+</details>
+
 `;
 
 });
 
-
 function peroxide(){
-peroxideResult.innerText=((ppm.value*vol.value)/(strength.value*10)).toFixed(2)+" mL";
+peroxideResult.innerText=((targetPPM.value*waterVol.value)/(peroxidePercent.value*10)).toFixed(2)+" mL";
+}
+
+function waterUse(){
+waterResult.innerText=(waterArea.value*waterRate.value).toFixed(0)+" L/day";
+}
+
+function waterStorage(){
+storageResult.innerText=(storage.value/days.value).toFixed(2)+" days supply";
 }

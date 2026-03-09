87e7f7fcc89cd6d54242510027ff27602eb92dcb
mwillis1989 <105248701+mwillis1989@users.noreply.github.com>
2026-03-09 12:31:55 -0700

    Update water.js

--- js/water.js
+++ js/water.js
@@ -1,21 +1,20 @@
 document.addEventListener("DOMContentLoaded",function(){
 
-const block=document.getElementById("calcBlock6");
-if(!block) return;
+const block=document.getElementById("calcBlock6")
 
 block.innerHTML=`
 
 <details>
 <summary>Hydrogen Peroxide Dilution</summary>
 
-<label>Water Volume (L)</label>
-<input id="vol">
-
-<label>H₂O₂ Strength %</label>
-<input id="strength">
+<label>Water L</label>
+<input id="waterVol">
 
 <label>Target ppm</label>
-<input id="ppm">
+<input id="targetPPM">
+
+<label>% peroxide</label>
+<input id="peroxidePercent">
 
 <button onclick="peroxide()">Calculate</button>
 
@@ -23,50 +22,13 @@ block.innerHTML=`
 
 </details>
 
+`
 
-<details>
-<summary>Irrigation Water Requirement</summary>
-
-<label>Area m²</label>
-<input id="areaW">
-
-<label>L per m²</label>
-<input id="rateW">
-
-<button onclick="waterUse()">Calculate</button>
-
-<div id="waterResult" class="result"></div>
-
-</details>
-
-
-<details>
-<summary>Annual Water Storage</summary>
-
-<label>Storage m³</label>
-<input id="storage">
-
-<label>Days</label>
-<input id="days">
-
-<button onclick="waterStorage()">Calculate</button>
-
-<div id="storageResult" class="result"></div>
-
-</details>
-
-`;
-
-});
+})
 
 function peroxide(){
-peroxideResult.innerText=((ppm.value*vol.value)/(strength.value*10)).toFixed(2)+" mL";
-}
 
-function waterUse(){
-waterResult.innerText=(areaW.value*rateW.value).toFixed(0)+" L/day";
-}
+peroxideResult.innerText=
+((targetPPM.value*waterVol.value)/(peroxidePercent.value*10)).toFixed(2)+" ml"
 
-function waterStorage(){
-storageResult.innerText=(storage.value/days.value).toFixed(2)+" days supply";
 }
